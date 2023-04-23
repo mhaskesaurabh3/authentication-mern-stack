6623946d852985f87eaba52780421f1441b1f89d
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-04-23 17:24:04 +0530

    User modal created in Server

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -13,53 +13,95 @@ const Login = () => {
   const registerUser = async (e) => {
     console.log('button clicked');
     e.preventDefault();
-    const response = await axios.post(
-      'http://localhost:1337/api/register',
-      JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      }),
-      {
-        headers: {
-          'Content-Type': 'application/json',
+    try {
+      const response = await axios.post(
+        'http://localhost:1337/api/register',
+        {
+          name: name,
+          email: email,
+          password: password,
         },
-      }
-    );
-    const data = await response.json();
-    console.log(data);
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      let data = await response.data;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={registerUser} className='login_container'>
-        <TextField
-          id='outlined-basic'
-          label='Username'
-          variant='outlined'
-          type='text'
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br />
-        <TextField
-          label='Email'
-          variant='outlined'
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <TextField
-          label='Password'
-          variant='outlined'
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <TextField type='submit' value='Register' />
-      </form>
+      <div className='w-full max-w-xs'>
+        <form
+          onSubmit={registerUser}
+          className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+        >
+          <div className='mb-4'>
+            <label
+              className='block text-gray-700 text-sm font-bold mb-2'
+              htmlFor='username'
+            >
+              Username
+            </label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              id='username'
+              type='text'
+              placeholder='Username'
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='mb-4'>
+            <label
+              className='block text-gray-700 text-sm font-bold mb-2'
+              htmlFor='email'
+            >
+              Email
+            </label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              id='email'
+              type='email'
+              placeholder='Email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='mb-6'>
+            <label
+              className='block text-gray-700 text-sm font-bold mb-2'
+              htmlFor='password'
+            >
+              Password
+            </label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
+              id='password'
+              type='password'
+              placeholder='******************'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className='flex items-center justify-between'>
+            <input
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+              type='submit'
+              value='Register'
+            />
+
+            <a
+              className='inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800'
+              href='#'
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -1,13 +1,49 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const User = require('./models/user.modal');
+const mongoose = require('mongoose');
 
-app.use(cors);
+mongoose.connect('mongodb://localhost:27017/full-mern-stack-video');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Connected to MongoDB!');
+});
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get('./api/register', (req, res) => {
-  res.send('Hello World');
+app.post('/api/register', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: 'ok' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (user) {
+      res.json({ status: 'ok', user: true });
+    } else {
+      res.json({ status: 'error', user: false });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(1337, () => {
