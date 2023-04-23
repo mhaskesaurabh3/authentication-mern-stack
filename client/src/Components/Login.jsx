e3cec85ab5eb51c08f1b5f6b3af22c8289c0c49a
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-04-23 17:29:29 +0530

    Minor Css changes

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -88,7 +88,7 @@ const Login = () => {
           </div>
           <div className='flex items-center justify-between'>
             <input
-              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+              className='bg-blue-500 hover:bg-blue-700 cursor-pointer text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
               type='submit'
               value='Register'
             />
