962618b5a09c495eb85c2f06537e5ad7f46f4037
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-04-25 12:24:17 +0530

    Sending jwt token when user logs in with google

--- server/index.js
+++ server/index.js
@@ -13,20 +13,58 @@ createUserTable();
 app.use(cors());
 app.use(express.json());
 
+// app.post('/api/register', async (req, res) => {
+//   try {
+//     const { name, email, password } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     // Insert a new user into the users table
+//     const queryText =
+//       "INSERT INTO users (name, email, password) VALUES ($1, $2, COALESCE($3, ''))";
+//     const values = [name, email, hashedPassword];
+//     await pool.query(queryText, values);
+
+//     res.json({ status: 'ok' });
+//   } catch (err) {
+//     res.json({ status: 'error', error: 'Duplicate email' });
+//   }
+// });
+
 app.post('/api/register', async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insert a new user into the users table
-    const queryText =
-      "INSERT INTO users (name, email, password) VALUES ($1, $2, COALESCE($3, ''))";
-    const values = [name, email, hashedPassword];
-    await pool.query(queryText, values);
+    // Check if the user already exists in the database
+    const userQueryText = 'SELECT * FROM users WHERE email = $1';
+    const userValues = [email];
+    const userResult = await pool.query(userQueryText, userValues);
 
-    res.json({ status: 'ok' });
+    if (userResult.rowCount > 0) {
+      // User already exists, so generate a JWT token and return it
+      const user = userResult.rows[0];
+      const payload = {
+        name: user.name,
+        email: user.email,
+      };
+      const token = jwt.sign(payload, secret);
+      return res.json({ status: 'ok', token });
+    } else {
+      // User doesn't exist, so insert a new user into the users table
+      const queryText =
+        "INSERT INTO users (name, email, password) VALUES ($1, $2, COALESCE($3, ''))";
+      const values = [name, email, hashedPassword];
+      await pool.query(queryText, values);
+
+      const payload = {
+        name: name,
+        email: email,
+      };
+      const token = jwt.sign(payload, secret);
+      return res.json({ status: 'ok', token });
+    }
   } catch (err) {
-    res.json({ status: 'error', error: 'Duplicate email' });
+    res.json({ status: 'error', error: 'Error registering user' });
   }
 });
 
