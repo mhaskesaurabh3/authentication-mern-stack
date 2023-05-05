5fc0603592ffac4798bc3fd1aa9ccd9230ae7276
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-05-05 18:03:01 +0530

    Youtube video section added

---
+++ client/src/Components/Pages/Video/YoutubeEmbed.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import './YoutubeEmbed.css';
+
+const YoutubeEmbed = ({ embedId }) => (
+  <div className='video-responsive   '>
+    <h2 className='text-left ml-3 mt-3 font-semibold'>Video</h2>
+    <div className='youtube_video flex justify-center'>
+      <iframe
+        className='!w-3/4 mt-5  '
+        width='500px'
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+        title='Embedded youtube'
+      />
+    </div>
+  </div>
+);
+
+export default YoutubeEmbed;
--- client/src/Components/ProductView/ProductView.jsx
+++ client/src/Components/ProductView/ProductView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import YoutubeEmbed from '../Pages/Video/YoutubeEmbed';
 import BreadCumb from './Breadcumb/BreadCumb';
 import ProductDetails from './ProductDetails/ProductDetails';
 import UserDetails from './UserDetails/UserDetails';
@@ -9,13 +10,17 @@ const ProductView = () => {
       <div className='breadcumb'>
         <BreadCumb />
       </div>
-      <div className='product_details ml-6 mt-5 rounded-tl-md	border border-br-light-grey flex w-full justify-start'>
+      <div className='product_details ml-6 mt-5 rounded-tl-md	border border-br-light-grey flex  justify-start'>
         <div className='w-3/5 text-left border border-br-light-grey'>
           <ProductDetails />
         </div>
         <div className='w-2/5 mt-5'>
           <UserDetails />
         </div>
+      </div>
+
+      <div className='youtube_video_embeded  ml-6 mt-5 rounded-tl-md h-96 bg-white	border border-br-light-grey '>
+        <YoutubeEmbed embedId={'dQw4w9WgXcQ'} />
       </div>
     </div>
   );
