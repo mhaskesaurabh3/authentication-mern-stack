33cf612f8a3eccdb1f9191ea9d0311d501b686cb
mhaskesaurabh3 <mhaskesaurabh3@gmail.com>
2023-05-09 13:48:47 +0530

    Profile and location data added

--- client/src/Components/ProductView/Location/UserLocation.jsx
+++ client/src/Components/ProductView/Location/UserLocation.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function UserLocation() {
+  const companyLocation = useSelector(
+    (state) => state.products?.productData?.company
+  );
+
   useEffect(() => {
     const ifameData = document.getElementById('iframeId');
-    const lat = 50.779729;
-    const lon = 6.100367;
+    const lat = companyLocation?.address?.latitude;
+    const lon = companyLocation?.address?.longitude;
     ifameData.src = `https://maps.google.com/maps?q=${lat},${lon}&hl=es;&output=embed`;
   }, []);
   return (
--- client/src/Components/Sidebar/SidebarProfile.jsx
+++ client/src/Components/Sidebar/SidebarProfile.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import userPic from '../../assets/user.jpg';
 
 const SidebarProfile = () => {
+  const profileData = useSelector((state) => state.products?.productData?.user);
+
   return (
     <div>
       <div className='flex w-full  mt-9'>
         <img
           className='rounded-full overflow-hidden w-14 h-14 object-cover'
-          src={userPic}
+          src={profileData?.profilePicture}
           alt='user'
         ></img>
         <div className='flex flex-col justify-start ml-3'>
           <span className='text-black font-semibold text-left block'>
-            Richard Anthony
+            {profileData?.firstName} {profileData?.lastName}
           </span>
-          <span className='text-black font-light text-left block'>CTO</span>
+          <span className='text-black font-light text-left block'>
+            {profileData?.position}
+          </span>
         </div>
       </div>
     </div>
